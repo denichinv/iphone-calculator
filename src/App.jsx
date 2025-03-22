700c8de2073255afb77f4bbfe1bb2e03f15d00d8
Vilizar Denichin <denichinv@gmail.com>
2025-03-22 17:35:43 +0000

    basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  function appendToDisplay() {
+    
+  }
 
+  function calculate() {
+    
+  }
+
+  function clearDisplay() {
+    
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div id="calculator">
+      <input type="text" placeholder="0"/>
+      <div id="keys">
+        <button onClick={appendToDisplay('+')}>+</button>
+        <button onClick={appendToDisplay('7')}>7</button>
+        <button onClick={appendToDisplay('8')}>8</button>
+        <button onClick={appendToDisplay('9')}>9</button>
+        <button onClick={appendToDisplay('-')}>-</button>
+        <button onClick={appendToDisplay('4')}>4</button>
+        <button onClick={appendToDisplay('5')}>5</button>
+        <button onClick={appendToDisplay('6')}>6</button>
+        <button onClick={appendToDisplay('*')}>*</button>
+        <button onClick={appendToDisplay('1')}>1</button>
+        <button onClick={appendToDisplay('2')}>2</button>
+        <button onClick={appendToDisplay('3')}>3</button>
+        <button onClick={appendToDisplay('/')}>/</button>
+        <button onClick={appendToDisplay('0')}>0</button>
+        <button onClick={appendToDisplay('.')}>.</button>
+        <button onClick={calculate()}>=</button>
+        <button onClick={clearDisplay()}>C</button>
+
+
+
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     </div>
     </>
   )
 }
