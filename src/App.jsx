5b204d6839b6ee49196b0bd34ff7b5e5feec413c
Vilizar Denichin <denichinv@gmail.com>
2025-03-23 16:54:33 +0000

    button edit on signs

--- src/App.jsx
+++ src/App.jsx
@@ -23,19 +23,22 @@ function App() {
         <button onClick={appendToDisplay('7')}>7</button>
         <button onClick={appendToDisplay('8')}>8</button>
         <button onClick={appendToDisplay('9')}>9</button>
-        <button onClick={appendToDisplay('-')}>-</button>
+        <button onClick={appendToDisplay('−')}>−</button>
         <button onClick={appendToDisplay('4')}>4</button>
         <button onClick={appendToDisplay('5')}>5</button>
         <button onClick={appendToDisplay('6')}>6</button>
-        <button onClick={appendToDisplay('*')}>*</button>
+        <button onClick={appendToDisplay('×')}>×</button>
         <button onClick={appendToDisplay('1')}>1</button>
         <button onClick={appendToDisplay('2')}>2</button>
         <button onClick={appendToDisplay('3')}>3</button>
-        <button onClick={appendToDisplay('/')}>/</button>
+        <button onClick={appendToDisplay('÷')}>÷</button>
         <button onClick={appendToDisplay('0')}>0</button>
         <button onClick={appendToDisplay('.')}>.</button>
         <button onClick={calculate()}>=</button>
-        <button onClick={clearDisplay()}>C</button>
+        <button onClick={clearDisplay()}>AC</button>
+        <button onClick={clearDisplay()}>%</button>
+        <button onClick={clearDisplay()}>±</button>
+        <button onClick={clearDisplay()}>🖩</button>
 
 
 
