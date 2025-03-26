ca35792e2b3cb47eb93b9bc11b143e8f1aa539c1
Vilizar Denichin <denichinv@gmail.com>
2025-03-26 21:28:35 +0000

    handle display function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react"
 
 
 function App() {
 
+  const [display,setDisplay] = useState('')
 
   
-  function appendToDisplay() {
-    
+  function handleDisplay(value) {
+    setDisplay((prev) => prev + value)
   }
 
   function calculate() {
@@ -19,33 +21,29 @@ function App() {
     <>
   
      <div className="bg-[hsl(0,0%,15%)] rounded-xl max-w-lg overflow-hidden " id="calculator">
-      <input className="w-full p-5 text-6xl bg-[hsl(0,0%,20%)] text-white " type="text" placeholder="0"/>
-      <div className=" grid grid-cols-4 gap-2 p-6" id="keys">
-        <button className="lighter-buttons" onClick={clearDisplay()}>AC</button>
-        <button className="lighter-buttons" onClick={clearDisplay()}>±</button>
-        <button className="lighter-buttons" onClick={clearDisplay()}>%</button>
-        <button className="operator" onClick={appendToDisplay('÷')}>÷</button>
-        <button onClick={appendToDisplay('7')}>7</button>
-        <button onClick={appendToDisplay('8')}>8</button>
-        <button onClick={appendToDisplay('9')}>9</button>
-        <button className="operator" onClick={appendToDisplay('×')}>×</button>
-        <button onClick={appendToDisplay('4')}>4</button>
-        <button onClick={appendToDisplay('5')}>5</button>
-        <button onClick={appendToDisplay('6')}>6</button>
-        <button className="operator"  onClick={appendToDisplay('−')}>−</button>
-        <button onClick={appendToDisplay('1')}>1</button>
-        <button onClick={appendToDisplay('2')}>2</button>
-        <button onClick={appendToDisplay('3')}>3</button>
-        <button className="operator" onClick={appendToDisplay('+')}>+</button>
-        <button >🖩</button>
-        <button onClick={appendToDisplay('0')}>0</button>
-        <button onClick={appendToDisplay('.')}>.</button>
-        <button className="operator" onClick={calculate()}>=</button>
-
-
-
-
-      </div>
+      <input className="w-full p-5 text-6xl bg-[hsl(0,0%,20%)] text-white " type="text" value={display} />
+      <div className="grid grid-cols-4 gap-2 p-6" id="keys">
+          <button className="lighter-buttons" onClick={clearDisplay}>AC</button>
+          <button className="lighter-buttons" onClick={() => handleDisplay("±")}>±</button>
+          <button className="lighter-buttons" onClick={() => handleDisplay("%")}>%</button>
+          <button className="operator" onClick={() => handleDisplay("÷")}>÷</button>
+          <button onClick={() => handleDisplay("7")}>7</button>
+          <button onClick={() => handleDisplay("8")}>8</button>
+          <button onClick={() => handleDisplay("9")}>9</button>
+          <button className="operator" onClick={() => handleDisplay("×")}>×</button>
+          <button onClick={() => handleDisplay("4")}>4</button>
+          <button onClick={() => handleDisplay("5")}>5</button>
+          <button onClick={() => handleDisplay("6")}>6</button>
+          <button className="operator" onClick={() => handleDisplay("−")}>−</button>
+          <button onClick={() => handleDisplay("1")}>1</button>
+          <button onClick={() => handleDisplay("2")}>2</button>
+          <button onClick={() => handleDisplay("3")}>3</button>
+          <button className="operator" onClick={() => handleDisplay("+")}>+</button>
+          <button>🖩</button>
+          <button onClick={() => handleDisplay("0")}>0</button>
+          <button onClick={() => handleDisplay(".")}>.</button>
+          <button className="operator" onClick={calculate}>=</button>
+        </div>
      </div>
     </>
   )
